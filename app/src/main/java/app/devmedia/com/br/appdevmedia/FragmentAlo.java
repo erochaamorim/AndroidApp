4a73257dd33040a750f98e83e090d3ea653e5c60
erick.amorim <erochaamorim@gmail.com>
2017-09-25 14:33:29 -0400

    Aula 57 concluída

---
+++ app/src/main/java/app/devmedia/com/br/appdevmedia/FragmentAlo.java
@@ -0,0 +1,30 @@
+package app.devmedia.com.br.appdevmedia;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by erick.amorim on 25/09/2017.
+ */
+
+public class FragmentAlo extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_alo, container, false);
+
+    }
+}
