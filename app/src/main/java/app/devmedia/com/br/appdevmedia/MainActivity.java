043e20c309543267341fe4f3439c4affb2403c7b
erick.amorim <erochaamorim@gmail.com>
2017-10-09 08:18:48 -0400

    Aula 76 concluída

--- app/src/main/java/app/devmedia/com/br/appdevmedia/MainActivity.java
+++ app/src/main/java/app/devmedia/com/br/appdevmedia/MainActivity.java
@@ -1,5 +1,8 @@
 package app.devmedia.com.br.appdevmedia;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,14 +15,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.holder.BadgeStyle;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
@@ -35,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
     protected ViewPager viewPager;
     protected Drawer drawer;
 
+    protected final static long ID_ND_FOOTER = 500;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -49,41 +57,87 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
-        AccountHeader headerResult = new AccountHeaderBuilder()
+        AccountHeader drawerHeader = new AccountHeaderBuilder()
                 .withActivity(this)
                 .withHeaderBackground(R.drawable.header)
                 .addProfiles(
                         new ProfileDrawerItem().withName("Erick Rocha Amorim").withEmail("erochaamorim@gmail.com").withIcon(getResources().getDrawable(R.drawable.e))
                 )
-                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
-                    @Override
-                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
-                        return false;
-                    }
-                })
                 .build();
 
-        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName(R.string.nav_produtos);
-        PrimaryDrawerItem item2 = new PrimaryDrawerItem().withIdentifier(2).withName(R.string.nav_perfi);
+        final PrimaryDrawerItem itemPerfil = new PrimaryDrawerItem()
+                .withIdentifier(2)
+                .withName(R.string.nav_perfi)
+                .withIcon(FontAwesome.Icon.faw_user);
+        final PrimaryDrawerItem itemProdutos = new PrimaryDrawerItem()
+                .withIdentifier(1)
+                .withName(R.string.nav_produtos)
+                .withIcon(FontAwesome.Icon.faw_shopping_cart)
+                .withBadge("43")
+                .withBadgeStyle(new BadgeStyle()
+                        .withTextColor(Color.WHITE)
+                        .withColorRes(R.color.md_orange_700));
+        final PrimaryDrawerItem itemCompras = new PrimaryDrawerItem()
+                .withName("Últimas Compras")
+                .withIcon(FontAwesome.Icon.faw_cart_arrow_down)
+                .withBadge("2")
+                .withBadgeStyle(new BadgeStyle()
+                        .withTextColor(Color.WHITE)
+                        .withColorRes(R.color.md_orange_700));
 
         drawer = new DrawerBuilder()
-                .withAccountHeader(headerResult)
+                .withAccountHeader(drawerHeader)
                 .withActivity(MainActivity.this)
                 .withToolbar(toolbar)
                 .addDrawerItems(
-                        item1,
-                        item2
+                        new SectionDrawerItem().withName("Conta do Usuário"),
+                        itemPerfil,
+                        new SectionDrawerItem().withName("Ações do Sistema"),
+                        itemProdutos,
+                        itemCompras
                 )
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        viewPager.setCurrentItem(position);
-                        drawer.closeDrawer();
+                        configurarItensDrawer(position, drawerItem );
                         return true;
                     }
                 })
                 .build();
+        drawer.addStickyFooterItem(new PrimaryDrawerItem()
+                .withName("Sobre o App")
+                .withIdentifier(ID_ND_FOOTER));
 
+    }
+
+    protected void configurarItensDrawer(int position, IDrawerItem drawerItem) {
+
+        switch((int) drawerItem.getIdentifier()) {
+
+            case 1:
+
+                viewPager.setCurrentItem(0);
+                break;
+
+            case 2:
+
+                viewPager.setCurrentItem(1);
+                break;
+
+            case (int) ID_ND_FOOTER:
+
+                String versao = "Indisponível";
+                try {
+                    PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), 0);
+                    versao = info.versionName;
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(MainActivity.this, "Versão: " + versao, Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        drawer.closeDrawer();
     }
 
     @Override
